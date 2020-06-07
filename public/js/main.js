f07502f35d89cc42034be21c0eb500feb72f87de
Martijn152 <m.dered@hotmail.com>
2020-06-07 17:07:26 +0200

    Added several hunt functions
    - Hunts are now displayed in a modal when clicked in main.html
    - The createhunts.html page now fetches locations and displays them in a list
    - Users can add hunts using the interface in createhunt.html

--- public/js/main.js
+++ public/js/main.js
@@ -40,58 +40,96 @@ function onRegisterClick(){
 }
 
 var huntList;
+var locationList;
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    console.log("if (user) was triggered");
-    // Now do things here
-    // Send user to main page
+    // User is logged in
     if(window.location.pathname == "/login.html"){
       window.location.replace("/main.html")
-    }
+    }else if (window.location.pathname == "/main.html") {
 
-    var huntUl = document.getElementById("hunt-ul");
+      // On main page we want to load the main page elements (such as hunt list)
+      var huntUl = document.getElementById("hunt-ul");
+      // Here we should load the user's hunts and display user specific information
+      db.collection("hunts").where("owner", "==", user.email)
+      .get()
+      .then(function(querySnapshot) {
+        huntList = [querySnapshot.size];
+        var i = 0;
+        querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
 
+          // Create a hunt using the document database
+          // Then add that to the list
 
-    // User is signed in.
-    // Here we should load the user's hunts and display user specific information
-    db.collection("hunts").where("owner", "==", user.email)
-    .get()
-    .then(function(querySnapshot) {
-      huntList = [querySnapshot.size];
-      var i = 0;
-      querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-
-        // Create a hunt using the document database
-        // Then add that to the list
-
-        var hunt = {
-          title: doc.id,
-          description: doc.data().description,
-          locations: doc.data().locations
-        }
+          var hunt = {
+            title: doc.id,
+            description: doc.data().description,
+            locations: doc.data().locations
+          }
 
 
-        var huntLi = document.createElement("li");
-        huntLi.appendChild(document.createTextNode(hunt.title))
-        huntLi.onclick = displayHunt(hunt.title);
-        huntUl.appendChild(huntLi);
+          var huntLi = document.createElement("li");
+          huntLi.appendChild(document.createTextNode(hunt.title))
+          huntLi.onclick = function() { displayHunt(hunt.title); };
+          //huntLi.value = "Kristianstad Top 10";
+          huntUl.appendChild(huntLi);
 
-        huntList[i] = hunt;
-        i++;
+          huntList[i] = hunt;
+          i++;
 
 
+        });
+      })
+      .catch(function(error) {
+        console.log("Error getting documents: ", error);
       });
-    })
-    .catch(function(error) {
-      console.log("Error getting documents: ", error);
-    });
 
+    }else if (window.location.pathname == "/createhunt.html") {
+      // Here we need to populate the locations list so the user can select some for their hunt
+      // On main page we want to load the main page elements (such as hunt list)
+      var locationUl = document.getElementById("location-ul");
+      // Here we should load the user's hunts and display user specific information
+      db.collection("locations")
+      .get()
+      .then(function(querySnapshot) {
+        locationList = [querySnapshot.size];
+        var i = 0;
+        querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
 
+          // Create a location using the document database
+          // Then add that to the list
 
+          var location = {
+            title: doc.id,
+            description: doc.data().description,
+            location: doc.data().location,
+            owner: doc.data().owner,
+            photo: doc.data().photo
+          }
 
+          var locationLi = document.createElement("li");
+          var checkbox = document.createElement("input");
+          checkbox.type = "checkbox";
+          checkbox.id = location.title + "checkbox";
+
+          locationLi.appendChild(checkbox);
+          locationLi.appendChild(document.createTextNode(location.title))
+
+          locationUl.appendChild(locationLi);
+
+          locationList[i] = location;
+          i++;
+        });
+      })
+      .catch(function(error) {
+        console.log("Error getting documents: ", error);
+      });
+    }
   } else {
     // User is signed out.
     // User should be redirected to login.html
@@ -102,23 +140,85 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
-
+// Displays a modal that shows information on the selected hunt
 function displayHunt(huntTitle){
+  //huntTitle = huntTitle.value;
   console.log("displayHunt was called");
-  huntList.forEach((item, i) => {
+  huntList.forEach(function(item, i){
     console.log("checking item: " + item);
     console.log("item has title " + item.title);
     console.log("huntTitle is " + huntTitle);
     if(item.title == huntTitle){
       var huntModal = document.getElementById("hunt-modal");
       huntModal.style.display='block';
+      // Set modal elements to display the right information
+      document.getElementById("modal-hunt-title").innerHTML = item.title;
+      document.getElementById("modal-hunt-description").innerHTML = item.description;
+      var locationUl = document.getElementById("modal-location-ul");
 
+
+      item.locations.forEach(function(item2, i2){
+        item2
+        .get()
+        .then(function(doc) {
+          if (doc.exists) {
+            console.log("Document data:", doc.data());
+            var locationLi = document.createElement("li");
+            locationLi.appendChild(document.createTextNode(doc.id));
+            locationUl.appendChild(locationLi);
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+        }).catch(function(error) {
+          console.log("Error getting document:", error);
+        });
+      });
     }
   });
+}
+
+function createHunt(){
+  // Get all of the information
+  // Then store it in the database
+  var title = document.getElementById("create-hunt-title").value;
+  console.log(title);
+  var description = document.getElementById("create-hunt-description").value;
+  console.log(description);
+
+  var locationsToAdd = [];
+  var user = firebase.auth().currentUser;
+  var j = 0;
+
+  // Run through locations
+  locationList.forEach(function(item, i){
+    var checkbox = document.getElementById(item.title + "checkbox");
+
+    // Check each checkbox
+    // If the box is checked the location should be added
+    if(checkbox.checked){
+      locationsToAdd[j] = db.doc("locations/" + item.title);
+      j++;
+    }
+  });
+
+  var docData = {
+    description: description,
+    locations: locationsToAdd,
+    owner: user.email
+  }
+
+  console.log(docData);
+
+  db.collection("hunts").doc(title).set(docData).then(function() {
+    console.log("Document successfully written!");
+  });
+
 
 
 }
 
+// Logs the user out
 function onLogoutClick(){
   firebase.auth().signOut().then(function() {
     // Sign-out successful.
